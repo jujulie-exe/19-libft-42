e50ae32d9dd9f90ae7b5c064d4ee17f6e81e623d
Julie Jeannine Hunter Franco <jfranco@fu-r7-p4.s19.be>
2024-10-22 10:31:24 +0200

    lilalu

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jfranco <jfranco@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 17:12:16 by jfranco           #+#    #+#             */
-/*   Updated: 2024/10/15 18:23:02 by jfranco          ###   ########.fr       */
+/*   Updated: 2024/10/22 10:28:44 by jfranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	total;
 	void	*call;
 
+	if (count == 0 || size == 0)
+		return (malloc(0));
 	if (size && count > SIZE_MAX / count)
 		return (NULL);
 	total = size * count;
